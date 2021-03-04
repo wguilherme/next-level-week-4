96af450868040b4e47970c3ace81c143071b2f51
W. Guilherme <ravok123@gmail.com>
2021-03-04 16:52:31 -0300

    [api] added controller and repository and create user + duplicated email validation

--- trilha-node/api/src/controllers/UserController.ts
+++ trilha-node/api/src/controllers/UserController.ts
@@ -1,12 +1,33 @@
 import {Request, Response} from "express"
+import { getRepository } from "typeorm";
+import { User } from "../models/User";
 
 class UserController {
 
 
    async create(req: Request, res: Response){
+      const {name, email} = req.body;
 
-      console.log(req.body)
-      return res.json(req.body)
+      
+      const usersRepository = getRepository(User)
+
+      // SELECT * FROM USERS WHERE EMAIL = "EMAIL"
+      const usersAlreadyExists = await usersRepository.findOne({
+         email
+      })
+
+      if(usersAlreadyExists){
+         return res.status(400).json({error: "User already exists"})
+      }
+
+      const user = usersRepository.create({
+         name,email
+      })
+
+      await usersRepository.save(user);
+
+
+      return res.status(201).json(user)
    }
 }
 
