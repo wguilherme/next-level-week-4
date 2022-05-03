fe0fd14c2f1f79c3082143fcae624cf4fcba35c2
W. Guilherme <wguilhermecriativo@gmail.com>
2022-05-03 16:09:36 -0300

    [api] add UserRespository

--- trilha-node/api/src/controllers/UserController.ts
+++ trilha-node/api/src/controllers/UserController.ts
@@ -1,6 +1,6 @@
-import {Request, Response} from "express"
-import { getRepository } from "typeorm";
-import { User } from "../models/User";
+import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { UsersRepository } from "../repositories/UserRepository";
 
 class UserController {
 
@@ -9,7 +9,7 @@ class UserController {
       const {name, email} = req.body;
 
       
-      const usersRepository = getRepository(User)
+      const usersRepository = getCustomRepository(UsersRepository)
 
       // SELECT * FROM USERS WHERE EMAIL = "EMAIL"
       const usersAlreadyExists = await usersRepository.findOne({
